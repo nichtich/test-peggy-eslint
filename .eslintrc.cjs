dac5994ed414773c1fb5ca7c2b1e31caa83dac8d
Jakob Voss <voss@gbv.de>
2024-03-02 20:54:19 +0100

    Change example to use semi rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
-console.log(require("@peggyjs/eslint-plugin").configs) // undefined?!
-
 module.exports = {
-  root: true, plugins: ["@peggyjs"], // these lines removed neither helps
   extends: ["plugin:@peggyjs/recommended"],
+  rules: {
+    semi: [ "error", "never" ], // Disallow semicolons
+  },
 };
